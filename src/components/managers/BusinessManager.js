e4c03f3c5d6c4250352a389b00a007aab2309240
Jonathan Woodard <jvw.226@gmail.com>
2023-02-27 15:17:53 -0600

    added new fetch calls to business Manager

--- src/components/managers/BusinessManager.js
+++ src/components/managers/BusinessManager.js
@@ -1,15 +1,12 @@
-// Interview Manager Component holds all Interview related requests.
+// Business Manager Component holds all Business related requests.
 
-export const createBusiness = (interview) => {
-  return fetch("http://localhost:8088/interviews", {
+export const createBusiness = (business) => {
+  return fetch("http://localhost:8088/business", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Token ${localStorage.getItem("seeker_token")}`,
-    },
-    body: JSON.stringify(interview),
+    body: JSON.stringify(business),
   }).then((res) => res.json());
 };
+
 
 export const getAllBusinesses = () => {
   return fetch(`http://localhost:8088/businesses?_expand=category`).then(
@@ -17,7 +14,7 @@ export const getAllBusinesses = () => {
   );
 };
 
-export const getSingleBusiness = ({ id }) => {
+export const getSingleBusiness = (id) => {
   return fetch(`http://localhost:8088/businesses/${id}?_expand=category`).then(
     (res) => res.json()
   );
@@ -43,15 +40,31 @@ export const createIncident = (newIncident) => {
   }).then(response => response.json())
 }
 
+export const createLike = (newLike) => {
+  return fetch(`http://localhost:8088/likes`, {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(newLike)
+  }).then(response => response.json())
+}
+
+export const getAllLikesForBusiness = (businessId) => {
+  return fetch(`http://localhost:8088/likes?businessId=${businessId}`).then((res) => res.json());
+};
 export const getAllIncidentTypes = () => {
   return fetch(`http://localhost:8088/incidentTypes`).then((res) => res.json());
 };
 
 export const deleteBusiness = (id) => {
-  return fetch(`http://localhost:8088/interviews/${id}`, {
+  return fetch(`http://localhost:8088/businesses/${id}`, {
     method: "DELETE",
-    headers: {
-      Authorization: `Token ${localStorage.getItem("seeker_token")}`,
-    },
+  });
+};
+
+export const deleteLike = (id) => {
+  return fetch(`http://localhost:8088/likes/${id}`, {
+    method: "DELETE",
   });
 };
